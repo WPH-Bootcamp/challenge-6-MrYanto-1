e0bd131c89fb82b3ea8066b3d159fa9d0b12ed1e
Yanto <yanto.santoso1@gmail.com>
2026-06-15 22:46:35 +0700

    added searchBook function

--- src/functions/bookManager.ts
+++ src/functions/bookManager.ts
@@ -29,3 +29,27 @@ function listBooks(): void {
 // Fungsi ini tidak mengembalikan nilai (void)
 // Petunjuk: jika parameter title diberikan, cari buku yang cocok
 //           jika tidak diberikan, tampilkan semua buku atau berikan informasi yang sesuai
+function searchBook(title?: string): void {
+  if (!title) {
+    console.log("No books found, here's the list of books you have:");
+    listBooks();
+    return;
+  }
+
+  const searchKeyword = title.toLowerCase();
+
+  const foundBook = Books.filter((book) =>
+    book.title.toLowerCase().includes(searchKeyword)
+  );
+
+  if (foundBook.length === 0) {
+    console.log(`No book found with the title containing "${searchKeyword}"`);
+  } else {
+    console.log(`Search result of ${searchKeyword}`);
+    foundBook.forEach((book, index) => {
+      console.log(
+        `${index + 1}. Title: ${book.title} | Author: ${book.author} | Year: ${book.publicationYear}`
+      );
+    });
+  }
+}
