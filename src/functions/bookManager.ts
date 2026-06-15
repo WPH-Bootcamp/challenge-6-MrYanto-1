c2e2e1c2ab7275e9372ef47869c71cf348713773
Yanto <yanto.santoso1@gmail.com>
2026-06-15 22:47:16 +0700

    exporting all of the function

--- src/functions/bookManager.ts
+++ src/functions/bookManager.ts
@@ -1,15 +1,12 @@
-import promptSync from 'prompt-sync';
 import type { Book } from './../types/index';
 import { Books } from '../data/books';
 
-const prompt = promptSync();
-
-function addBook(prop: Book): void {
+export function addBook(prop: Book): void {
   Books.push(prop);
   console.log(`${prop.title} has been added to the list`);
 }
 
-function listBooks(): void {
+export function listBooks(): void {
   if (Books.length === 0) {
     console.log("You don't have any book");
   } else {
@@ -23,13 +20,7 @@ function listBooks(): void {
   }
 }
 
-// Fungsi searchBook
-// Fungsi ini digunakan untuk mencari buku berdasarkan judul
-// Parameter title bersifat opsional (bisa ada atau tidak)
-// Fungsi ini tidak mengembalikan nilai (void)
-// Petunjuk: jika parameter title diberikan, cari buku yang cocok
-//           jika tidak diberikan, tampilkan semua buku atau berikan informasi yang sesuai
-function searchBook(title?: string): void {
+export function searchBook(title?: string): void {
   if (!title) {
     console.log("No books found, here's the list of books you have:");
     listBooks();
