29db32106a93b241ac40f18c25174f1dc310fda2
Yanto <yanto.santoso1@gmail.com>
2026-06-15 22:05:00 +0700

    added addBook function

--- src/functions/bookManager.ts
+++ src/functions/bookManager.ts
@@ -1,11 +1,13 @@
-// Tugas 3: Implementasikan fungsi-fungsi manajemen buku
+import promptSync from 'prompt-sync';
+import type { Book } from './../types/index';
+import { Books } from '../data/books';
 
-// Fungsi addBook
-// Fungsi ini digunakan untuk menambahkan buku baru ke dalam koleksi
-// Parameter yang dibutuhkan: data buku sesuai tipe Book
-// Fungsi ini tidak mengembalikan nilai (void)
-// Petunjuk: pikirkan bagaimana cara menambahkan buku ke array yang sudah disediakan
+const prompt = promptSync();
 
+function addBook(prop: Book): void {
+    Books.push(prop)
+    console.log(`${prop.title} has been added to the list`)
+}
 
 // Fungsi listBooks
 // Fungsi ini digunakan untuk menampilkan semua buku yang tersimpan
@@ -13,11 +15,9 @@
 // Fungsi ini tidak mengembalikan nilai (void)
 // Petunjuk: pikirkan cara menampilkan data buku dengan format yang mudah dibaca
 
-
 // Fungsi searchBook
 // Fungsi ini digunakan untuk mencari buku berdasarkan judul
 // Parameter title bersifat opsional (bisa ada atau tidak)
 // Fungsi ini tidak mengembalikan nilai (void)
 // Petunjuk: jika parameter title diberikan, cari buku yang cocok
 //           jika tidak diberikan, tampilkan semua buku atau berikan informasi yang sesuai
-
